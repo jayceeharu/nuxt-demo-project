2e8c804425201371e15af293e7b868852ad53cca
James Delos Reyes <james.delosreyes@awsys-i.com>
2022-03-08 10:55:59 +0800

    add data.js file

---
+++ assets/data.js
@@ -0,0 +1,47 @@
+export const realEstateData = [
+  {
+    id: 1,
+    hotelName: "HOTEL GLOBAL VIEW Hachinohe 102",
+    hotelAddress: "7 Jusannichimachi, Hachinohe City, Aomori Prefecture",
+
+    hotelDetails: [
+      {
+        id: 19,
+        rentCost: "Monthly fee 14.59 million yen including tax",
+        rentManagementFee: "",
+        rentMoney: "",
+        propertyNo: "00000000000192810003",
+        propertyType: "Rental stores (partial)",
+        propertyBldgStructure:
+          "Steel-framed reinforced concrete structure (SRC structure) / 9 floors above ground and 2 floors below ground",
+        propertyFlrNo: "Room 102 on the 1st floor",
+        propertyArea: "48.71㎡",
+        propertyTsubo: "14.73 tsubo",
+        propertyConstDate: "November 1980",
+        propertyParking: "Nothing",
+        propertyContract: "Ordinary rented house",
+        propertyContractPeriod: "2 years",
+        propertyCurrent: "Sky",
+        propertyDelivery: "",
+        propertyRenewalFee: "none",
+        propertyTransFee: "",
+      },
+      {
+        id: 20,
+        applicableIndustry: ApplicableIndustry,
+      },
+      {
+        id: 21,
+        features: Feature
+      },
+      {
+        id: 22,
+        location: Location
+      },
+      {
+        id: 23,
+        remarks: ""
+      },
+    ],
+  },
+];
